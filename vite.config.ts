0b87386da75394aa9940a32c88f5c2139caae28b
jekabskarklins <jekabs.karklins@ess.eu>
2026-04-07 15:40:59 +0200

    fix: set base to /

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/nmx_view/',
-  plugins: [react()],
+plugins: [react()],
   optimizeDeps: {
     exclude: ['h5wasm'],
   },
